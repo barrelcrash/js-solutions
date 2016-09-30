a18e92b15f84ce6328c61c7d8a0243b2a98b9ebe
Luke Abel <luke.abel@gmail.com>
2016-09-29 21:14:59 -0400

    eloquentJS-08-lockbox

---
+++ eloquentJS_08_lockbox.js
@@ -0,0 +1,40 @@
+var box = {
+  locked: true,
+  unlock: function() { this.locked = false; },
+  lock: function() { this.locked = true;  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  }
+};
+
+function withBoxUnlocked(body) {
+  var wasLocked = box.locked;
+  try {
+  	box.unlock();
+  	body();
+  } finally {
+  	if (wasLocked) {
+  	  console.log('Locking box.');
+  	  box.lock();
+  	}
+  }
+}
+
+withBoxUnlocked(function() {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function() {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised:", e);
+}
+console.log(box.locked);
+// → true
+
+box.unlock()
+console.log(box.content);
